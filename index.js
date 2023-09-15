963daf7bb6415e71368ee2da05de58550cdb9ab6
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-09-14 19:27:02 -0700

    :ambulance: Use inquirer 8.0.0 version

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const { startServer } = require('./hub.js');
+const inquirer = require('inquirer');
+
+function login() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your username:',
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Enter your password:',
+      mask: '*',
+    },
+  ]);
+}
+
+login();
 
 startServer();
