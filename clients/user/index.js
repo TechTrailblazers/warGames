4175953d34b980511774eeb301ed3d3611dd09d8
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-09-14 20:10:04 -0700

    combined files to one start point

--- clients/user/index.js
+++ clients/user/index.js
@@ -1,7 +1,6 @@
 const { startDriver } = require('./handler.js');
 const { io } = require('socket.io-client');
-// const { attackStarting } = require('./handler');
+
 const client = io('ws://localhost:3000');
 
 startDriver(client);
-// attackStarting(client);
